66ba54a5dd44356abcca0e8c9ac6db222083dfb8
Lucas <lucas@agtlucas.com>
2016-02-10 21:54:15 -0200

    Prepare markup

--- USDBRL/ViewController.swift
+++ USDBRL/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var inputValue: UITextField!
+    @IBOutlet weak var theResult: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func converter(sender: UIButton) {
+        theResult.text = inputValue.text
+    }
 
 }
 
